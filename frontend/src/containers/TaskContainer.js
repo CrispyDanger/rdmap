84280f907973dad15f8e5007be69f1c04447a085
CrispyDanger <crispyd2405@gmail.com>
2023-03-29 20:54:14 +0500

    Add basic drag and drop

--- frontend/src/containers/TaskContainer.js
+++ frontend/src/containers/TaskContainer.js
@@ -1,11 +1,38 @@
 import React, { Fragment } from 'react'
-import {DragDropContext, Droppable} from 'react-beautiful-dnd'
+import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd'
 
 
+const tasks = [
+  {
+    id: "task1",
+    name: "Test",
+    color: "bg-red-500"
+  },
+  {
+    id: "task2",
+    name: "Test2",
+    color: "bg-slate-600"
+  },
+  {
+    id: "task3",
+    name: "Test3",
+    color: "bg-amber-500"
+  },
+  {
+    id: "task4",
+    name: "Test4",
+    color: "bg-amber-400"
+  },
+]
+
+
+
+
+//TODO FIX DRAG AND DROP
 const TaskContainer = () => {
   return (
     <DragDropContext>
-    <div className='relative flex min-h-screen flex-col justify-top px-3 rounded-md'>
+  <div className='relative flex min-h-screen flex-col justify-top px-3 rounded-md'>
     <table className='bg-slate-50 border-gray border-1 border-slate-600 border-spacing-6   shadow-xl border-separate'>
       <thead>
         <tr>
@@ -14,24 +41,29 @@ const TaskContainer = () => {
             <th>Complete</th>
         </tr>
       </thead>
-        <Droppable>
         <tbody>
-      <tr>
-      <th className=' bg-red-500 rounded-md items-center h-24 border'>
-        <Fragment>Test</Fragment>
-      </th>
-      <th className='bg-slate-600 rounded-md border'>rd</th>
-      <th className='bg-amber-500 rounded-md border'>fsdfs</th>
-      </tr>
-      <tr>
-        <th className='bg-amber-400 rounded-md h-24 border'>Rpr</th>
-      </tr>
-      </tbody>
-      </Droppable>
-    </table>
-    </div>
-    </DragDropContext>
-  )
+      <Droppable droppableId='tasks'>
+        {(provided) => (
+      <tr {...provided.droppableProps} ref={provided.innerRef}>
+        {provided.placeholder}
+      {tasks.map(({id, name, color}, index) => {
+        return (
+          <Draggable key={id} draggableId={id} index={index}>
+            {(provided) => (
+          <th className={`${color} rounded-md items-center h-24 border`} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+            <div>{name}</div>
+          </th>
+            )}
+          </Draggable>
+          );
+        })}
+        </tr>
+        )}
+        </Droppable>
+        </tbody>
+        </table>
+        </div>
+        </DragDropContext>
+    )
 }
-
 export default TaskContainer
